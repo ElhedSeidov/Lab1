c853fe95f0ce5f5e04fd109f7516d1eeac759090
Seyidov Alhad <alhadss@code.local>
2023-07-28 19:51:23 +0400

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name1 = "Elhed";
+            string name = "hello";
         }
     }
 }
